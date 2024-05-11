a57abb311c6915d00fadc9e97b9c54a8037f905c
vnewww <126395918+vnewww@users.noreply.github.com>
2024-05-11 16:33:43 +0800

    Add files via upload

---
+++ admin-inventory-dtb/update_reservation.php
@@ -0,0 +1,30 @@
+<?php
+@include 'config.php';
+
+if(isset($_POST['update_reservation'])){
+  $id = $_POST['id'];
+  $reservation_name = $_POST['reservation'];
+  $customer = $_POST['customer'];
+  $product = $_POST['product'];
+  $reserve_units = $_POST['reserved_units'];
+  $description = $_POST['description'];
+  $status = $_POST['status'];
+  
+  $sql = "UPDATE `reservation` SET `reservation_name`='$reservation_name',`customer`='$customer',`product`='$product',`reserved_units`='$reserve_units', `status`='$status' WHERE id = $id";
+  
+
+  $update = mysqli_query($conn, $sql);
+  
+  if($update){
+    echo "
+    <script>alert('Update data successfully.')</script>
+    ";
+    header("Location: index.php");
+  }
+  
+}
+
+
+
+
+?>
