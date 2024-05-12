b29f07488f9ddca795b90fe2901d4c783e9a7f66
vnewww <126395918+vnewww@users.noreply.github.com>
2024-05-12 21:16:18 +0800

    Update admin-inventory.php

--- admin-inventory.php
+++ admin-inventory.php
@@ -17,8 +17,8 @@
     <!---------------
          CSS & JS
     ---------------->
-    <link rel="stylesheet" href="css/mainstyle.css">
     <link rel="stylesheet" href="css/admin-inventory.css">
+      <link rel="stylesheet" href="css/admin-mainstyle.css">
     <script src="js/mainscript.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
