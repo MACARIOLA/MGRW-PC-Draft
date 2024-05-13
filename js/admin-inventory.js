327f29d9abbc80a1190d81b6ae652a7ff6b1b7a6
vnewww <126395918+vnewww@users.noreply.github.com>
2024-05-13 19:47:13 +0800

    Update admin-inventory.js

--- js/admin-inventory.js
+++ js/admin-inventory.js
@@ -3,91 +3,91 @@
 function searchTable() {
 
 
-    var input, filter, table, tbody, tr, td, i, j, txtValue;
-    input = document.getElementById("inventorySearchInput"); 
-    filter = input.value.toUpperCase();
-    table = document.getElementById("inventoryTable");
-    tbody = table.getElementsByTagName("tbody");
-    tr = tbody[0].getElementsByTagName("tr");
+        var input, filter, table, tbody, tr, td, i, j, txtValue;
+        input = document.getElementById("inventorySearchInput"); 
+        filter = input.value.toUpperCase();
+        table = document.getElementById("inventoryTable");
+        tbody = table.getElementsByTagName("tbody");
+        tr = tbody[0].getElementsByTagName("tr");
 
 
-    for (i = 0; i < tr.length; i++) {
-        var found = false;
+        for (i = 0; i < tr.length; i++) {
+            var found = false;
 
-        for (j = 0; j < tr[i].cells.length; j++) {
-            td = tr[i].cells[j];
-            if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    found = true;
-                    break; 
+            for (j = 0; j < tr[i].cells.length; j++) {
+                td = tr[i].cells[j];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        found = true;
+                        break; 
+                    }
                 }
             }
-        }
 
-        if (found) {
-            tr[i].style.display = "";
-        } else {
-            tr[i].style.display = "none";
+            if (found) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
         }
     }
-}
-
-
+    
+    
 //search for reservation
 
 function searchTable2() {
 
-    
-    var input, filter, table, tbody, tr, td, i, j, txtValue;
-    input = document.getElementById("reservationSearchInput"); 
-    filter = input.value.toUpperCase();
-    table = document.getElementById("reservationTable"); 
-    tbody = table.getElementsByTagName("tbody");
-    tr = tbody[0].getElementsByTagName("tr");
+        
+        var input, filter, table, tbody, tr, td, i, j, txtValue;
+        input = document.getElementById("reservationSearchInput"); 
+        filter = input.value.toUpperCase();
+        table = document.getElementById("reservationTable"); 
+        tbody = table.getElementsByTagName("tbody");
+        tr = tbody[0].getElementsByTagName("tr");
 
-    for (i = 0; i < tr.length; i++) {
-        var found = false;
-      
-        for (j = 0; j < tr[i].cells.length; j++) {
-            td = tr[i].cells[j];
-            if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    found = true;
-                    break; 
+        for (i = 0; i < tr.length; i++) {
+            var found = false;
+          
+            for (j = 0; j < tr[i].cells.length; j++) {
+                td = tr[i].cells[j];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        found = true;
+                        break; 
+                    }
                 }
             }
+            if (found) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
         }
-        if (found) {
-            tr[i].style.display = "";
-        } else {
-            tr[i].style.display = "none";
-        }
     }
-}
-
-
-
-
-
-
-
-
-    function showPreview() {
-        document.getElementById("preview").style.display = "block";
-    }
-
-    function hidePreview() {
-        document.getElementById("preview").style.display = "none";
-    }
-
     
+
+
+
+
+
+
+
+        function showPreview() {
+            document.getElementById("preview").style.display = "block";
+        }
+
+        function hidePreview() {
+            document.getElementById("preview").style.display = "none";
+        }
+
+        
 function toggleClick() {
-const navContainer = document.getElementById("nav-container");
-if (navContainer.style.left === '-800px') {
-navContainer.style.left = '0';
-} else {
-navContainer.style.left = '-800px';
-}
+  const navContainer = document.getElementById("nav-container");
+  if (navContainer.style.left === '-800px') {
+    navContainer.style.left = '0';
+  } else {
+    navContainer.style.left = '-800px';
+  }
 }
