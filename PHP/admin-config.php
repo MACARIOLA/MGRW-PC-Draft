935fc785f8fdcfd02530a016024d85e029569617
11Boomie <154297153+11Boomie@users.noreply.github.com>
2024-05-14 23:43:16 +0800

    Update admin-config.php

--- PHP/admin-config.php
+++ PHP/admin-config.php
@@ -1,3 +1,11 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'admin-inventory');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mgwrpcdtb";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 ?>
