d91db8b6a053bd462b57ed7d96f20f4c728f7e18
Joshua Alen Lopez <118281707+Joshieepooo@users.noreply.github.com>
2024-05-15 13:21:01 +0800

    Update MgwrPcDtb.sql

--- Dtb/MgwrPcDtb.sql
+++ Dtb/MgwrPcDtb.sql
@@ -1,72 +1,55 @@
------------------------------------
--- Create the MgwrPcDtb database --
------------------------------------
+-- ------------------------------- --
+-- -< CREATING MGWR PC DATABASE >- --
 CREATE DATABASE IF NOT EXISTS MgwrPcDtb;
 USE MgwrPcDtb;
 
+-- -< CREATING MGWR PC DATABASE >- --
+-- ------------------------------- --
 
 
---------------------------------
--- Reservation Contacts Table --
---------------------------------
-CREATE TABLE IF NOT EXISTS ReservationContactsTbl (
-    ReservationID INT PRIMARY KEY,
-    Name VARCHAR(255),
-    Email VARCHAR(255),
-    ContactNumber VARCHAR(11) CHECK (ContactNumber LIKE '09_________')
+
+
+
+-- ------------------------------- --
+-- ------------< CMS >------------ --
+
+-- PROMOTIONS --
+CREATE TABLE `cms_home-promotions` (
+  `ID` int(2) NOT NULL,
+  `PHOTO` longblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `cms_home-promotions` (`ID`, `PHOTO`) VALUES
+(1, NULL),
+(2, NULL),
+(3, NULL);
+
+ALTER TABLE `cms_home-promotions`
+  ADD PRIMARY KEY (`ID`);
+COMMIT;
+
+
+
+-- PDF FILES TABLE --
+CREATE TABLE `cms_pricelist-pdfdownloads` (
+    `PdfID` INT AUTO_INCREMENT PRIMARY KEY,
+    `FileName` VARCHAR(255) NOT NULL,
+    `FilePath` VARCHAR(255) NOT NULL,
+    `Description` TEXT
 );
 
-
------------------------------
--- Product Comments Table --
------------------------------
-CREATE DATABASE IF NOT EXISTS mgwrpcdtb;
-USE mgwrpcdtb;
-
-CREATE TABLE IF NOT EXISTS tbl_survey_responses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    age INT,
-    experience VARCHAR(255),
-    liked_feature VARCHAR(255),
-    recommend VARCHAR(255),
-    sensible_part VARCHAR(255),
-    improvement_suggestion VARCHAR(255),
-    device_impact VARCHAR(255),
-    likelihood_of_return VARCHAR(255),
-    likelihood_of_purchase VARCHAR(255),
-    overall_performance VARCHAR(255),
-    comment TEXT
-);
+-- ------------< CMS >------------ --
+-- ------------------------------- --
 
 
 
------------------------------
--- Product Comments Table --
------------------------------
-CREATE TABLE IF NOT EXISTS ProductComments (
-    CommentID INT PRIMARY KEY AUTO_INCREMENT,
-    ProductID INT,
-    Name VARCHAR(255),
-    Comment TEXT,
-    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
-);
 
 
+-- ------------------------------- --
+-- -----------< FORMS >----------- --
 
----------------------
--- PDF Files Table --
----------------------
-CREATE TABLE IF NOT EXISTS PdfFiles (
-    PdfID INT PRIMARY KEY AUTO_INCREMENT,
-    FileName VARCHAR(255) NOT NULL,
-    FilePath VARCHAR(255) NOT NULL,
-    Description TEXT
-);
-
-
-CREATE TABLE `reviews` (
+-- PRODUCT REVIEWS --
+CREATE TABLE `customer-product-reviews` (
   `submission_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -79,3 +62,64 @@ CREATE TABLE `reviews` (
   `gift_shopper` tinyint(1) DEFAULT 0,
   `not_interested` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `customer-product-reviews`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `customer-product-reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
+
+
+
+-- SURVEY RESPONSES --
+CREATE TABLE `customer-survey-responses` (
+  `submission_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id` INT NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `age` INT NULL,
+  `experience` VARCHAR(255) NOT NULL,
+  `liked_feature` VARCHAR(255) NOT NULL,
+  `recommend` VARCHAR(255) NOT NULL,
+  `sensible_part` VARCHAR(255) NOT NULL,
+  `improvement_suggestion` VARCHAR(255) NOT NULL,
+  `device_impact` VARCHAR(255) NOT NULL,
+  `likelihood_of_return` VARCHAR(255) NOT NULL,
+  `likelihood_of_purchase` VARCHAR(255) NOT NULL,
+  `overall_performance` VARCHAR(255) NOT NULL,
+  `comment` TEXT NOT NULL
+);
+
+ALTER TABLE `customer-survey-responses`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `customer-survey-responses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
+
+-- -----------< FORMS >----------- --
+-- ------------------------------- --
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- -------------------------- --
+-- Reservation Contacts Table --
+-- -------------------------- --
+CREATE TABLE IF NOT EXISTS ReservationContactsTbl (
+    ReservationID INT PRIMARY KEY,
+    Name VARCHAR(255),
+    Email VARCHAR(255),
+    ContactNumber VARCHAR(11) CHECK (ContactNumber LIKE '09_________')
+);
