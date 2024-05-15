61f197f769cb4f8e395c85e034250a78147abc8d
Joshua Alen Lopez <118281707+Joshieepooo@users.noreply.github.com>
2024-05-15 13:35:15 +0800

    Update MgwrPcDtb.sql

--- Dtb/MgwrPcDtb.sql
+++ Dtb/MgwrPcDtb.sql
@@ -14,28 +14,28 @@ USE MgwrPcDtb;
 -- ------------< CMS >------------ --
 
 -- PROMOTIONS --
-CREATE TABLE `cms_home-promotions` (
-  `ID` int(2) NOT NULL,
-  `PHOTO` longblob DEFAULT NULL
+CREATE TABLE IF NOT EXISTS cms_home_promotions (
+  ID int(2) NOT NULL,
+  PHOTO longblob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `cms_home-promotions` (`ID`, `PHOTO`) VALUES
+INSERT INTO `cms_home_promotions` (`ID`, `PHOTO`) VALUES
 (1, NULL),
 (2, NULL),
 (3, NULL);
 
-ALTER TABLE `cms_home-promotions`
+ALTER TABLE `cms_home_promotions`
   ADD PRIMARY KEY (`ID`);
 COMMIT;
 
 
 
 -- PDF FILES TABLE --
-CREATE TABLE `cms_pricelist-pdfdownloads` (
-    `PdfID` INT AUTO_INCREMENT PRIMARY KEY,
-    `FileName` VARCHAR(255) NOT NULL,
-    `FilePath` VARCHAR(255) NOT NULL,
-    `Description` TEXT
+CREATE TABLE IF NOT EXISTS cms_pricelist_pdfdownloads (
+    PdfID INT AUTO_INCREMENT PRIMARY KEY,
+    FileName VARCHAR(255) NOT NULL,
+    FilePath VARCHAR(255) NOT NULL,
+    Description TEXT
 );
 
 -- ------------< CMS >------------ --
@@ -46,55 +46,65 @@ CREATE TABLE `cms_pricelist-pdfdownloads` (
 
 
 -- ------------------------------- --
+-- ---------< INVENTORY >--------- --
+
+-- ---------< INVENTORY >--------- --
+-- ------------------------------- --
+
+
+
+
+
+-- ------------------------------- --
 -- -----------< FORMS >----------- --
 
 -- PRODUCT REVIEWS --
-CREATE TABLE `customer-product-reviews` (
-  `submission_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `comment` text DEFAULT NULL,
-  `rating` enum('5 Star','4 Star','3 Star','2 Star','1 Star') NOT NULL,
-  `first_time_buyer` tinyint(1) DEFAULT 0,
-  `regular_customer` tinyint(1) DEFAULT 0,
-  `budget_shopper` tinyint(1) DEFAULT 0,
-  `brand_loyalist` tinyint(1) DEFAULT 0,
-  `gift_shopper` tinyint(1) DEFAULT 0,
-  `not_interested` tinyint(1) DEFAULT 0
+CREATE TABLE IF NOT EXISTS customer_product_reviews (
+  submission_date timestamp NOT NULL DEFAULT current_timestamp(),
+  id int(11) NOT NULL,
+  name varchar(255) NOT NULL,
+  comment text DEFAULT NULL,
+  rating enum('5 Star','4 Star','3 Star','2 Star','1 Star') NOT NULL,
+  first_time_buyer tinyint(1) DEFAULT 0,
+  regular_customer tinyint(1) DEFAULT 0,
+  budget_shopper tinyint(1) DEFAULT 0,
+  brand_loyalist tinyint(1) DEFAULT 0,
+  gift_shopper tinyint(1) DEFAULT 0,
+  not_interested tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `customer-product-reviews`
+ALTER TABLE `customer_product_reviews`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `customer-product-reviews`
+ALTER TABLE `customer_product_reviews`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
 
 
 -- SURVEY RESPONSES --
-CREATE TABLE `customer-survey-responses` (
-  `submission_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `id` INT NOT NULL,
-  `name` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `age` INT NULL,
-  `experience` VARCHAR(255) NOT NULL,
-  `liked_feature` VARCHAR(255) NOT NULL,
-  `recommend` VARCHAR(255) NOT NULL,
-  `sensible_part` VARCHAR(255) NOT NULL,
-  `improvement_suggestion` VARCHAR(255) NOT NULL,
-  `device_impact` VARCHAR(255) NOT NULL,
-  `likelihood_of_return` VARCHAR(255) NOT NULL,
-  `likelihood_of_purchase` VARCHAR(255) NOT NULL,
-  `overall_performance` VARCHAR(255) NOT NULL,
-  `comment` TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS customer_survey_responses (
+  submission_date timestamp NOT NULL DEFAULT current_timestamp(),
+  id INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  age INT NULL,
+  experience VARCHAR(255) NOT NULL,
+  liked_feature VARCHAR(255) NOT NULL,
+  recommend VARCHAR(255) NOT NULL,
+  sensible_part VARCHAR(255) NOT NULL,
+  improvement_suggestion VARCHAR(255) NOT NULL,
+  device_impact VARCHAR(255) NOT NULL,
+  likelihood_of_return VARCHAR(255) NOT NULL,
+  likelihood_of_purchase VARCHAR(255) NOT NULL,
+  overall_performance VARCHAR(255) NOT NULL,
+  comment TEXT NOT NULL
 );
 
-ALTER TABLE `customer-survey-responses`
+ALTER TABLE `customer_survey_responses`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `customer-survey-responses`
+ALTER TABLE `customer_survey_responses`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
