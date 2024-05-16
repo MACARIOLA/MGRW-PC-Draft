0a7939b47b9007c799c520a4cfef1e9273929680
Joshua Alen Lopez <118281707+Joshieepooo@users.noreply.github.com>
2024-05-16 13:29:08 +0800

    Update delete_feedback.php

--- PHP/delete_feedback.php
+++ PHP/delete_feedback.php
@@ -1,42 +1,37 @@
 <?php
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
-
-    // Log errors to a file
     ini_set('log_errors', 1);
     ini_set('error_log', '/var/log/php_errors.log');
 
 
-if (isset($_POST['id'])) {
-    $id = intval($_POST['id']);
-    echo "ID: " . $id;
+    if (isset($_POST['id'])) {
+        $id = intval($_POST['id']);
+        echo "ID: " . $id;
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "mgwrpcdtb";
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "mgwrpcdtb";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
-    // Delete the feedback from the database
-    $sql = "DELETE FROM customer_product_reviews WHERE id = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $id);
-    if ($stmt->execute()) {
-        echo "Feedback deleted successfully";
+        $sql = "DELETE FROM customer_product_reviews WHERE id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $id);
+        if ($stmt->execute()) {
+            echo "Feedback deleted successfully";
+        } else {
+            echo "Error deleting feedback: " . $conn->error;
+        }
+
+        $stmt->close();
+        $conn->close();
     } else {
-        echo "Error deleting feedback: " . $conn->error;
+        echo "No ID provided";
     }
-
-    $stmt->close();
-    $conn->close();
-} else {
-    echo "No ID provided";
-}
 ?>
