9919bfedbafde3aa1b9bc60f9f9dc0210b8b772f
11Boomie <154297153+11Boomie@users.noreply.github.com>
2024-05-16 22:09:41 +0800

    Update admin-update.php

--- PHP/admin-update.php
+++ PHP/admin-update.php
@@ -5,29 +5,29 @@ if(isset($_POST['update_product'])){
    $product_id = $_POST['id'];
    $product_products_id = $_POST['product_products_id']; 
    $product_name = $_POST['product_name'];
-   $product_image = $_FILES['product_image']['name'];
-   $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
-   $product_image_folder = 'uploaded_img/'.$product_image;
+   $product_image = $_FILES['product_image']['tmp_name'];
+   $product_image_size = $_FILES['product_image']['size'];
    $product_total_units = $_POST['product_total_units'];
    $product_reserved_units = $_POST['product_reserved_units'];
 
+   // Check if image file is uploaded
+   if (isset($_FILES['product_image']) && is_uploaded_file($_FILES['product_image']['tmp_name'])) {
+       // Read the image file
+       $imgContent = addslashes(file_get_contents($_FILES['product_image']['tmp_name']));
 
-   if(!empty($product_image)){
-         $update_data = "UPDATE inventory SET products_id='$product_products_id', products_name='$product_name', image='$product_image',
-         total_units='$product_total_units', reserved_units='$product_reserved_units' WHERE id = '$product_id'";
-      }else{
-         $update_data = "UPDATE inventory SET products_id='$product_products_id', products_name='$product_name',
-        
-          total_units='$product_total_units', reserved_units='$product_reserved_units' WHERE id = '$product_id'";
-      }
-      $upload = mysqli_query($conn, $update_data);
-      if($upload){
-         if(!empty($product_image)){
-            move_uploaded_file($product_image_tmp_name, $product_image_folder);
-         }
-         header("Location: admin-inventory.php");
-      }
+       // Update SQL query with image data
+       $update_data = "UPDATE inventory SET products_id='$product_products_id', products_name='$product_name', image='$imgContent', total_units='$product_total_units', reserved_units='$product_reserved_units' WHERE id = '$product_id'";
+   } else {
+       // If no image uploaded, update SQL query without image data
+       $update_data = "UPDATE inventory SET products_id='$product_products_id', products_name='$product_name', total_units='$product_total_units', reserved_units='$product_reserved_units' WHERE id = '$product_id'";
    }
 
+   // Execute the update query
+   $upload = mysqli_query($conn, $update_data);
+
+   if($upload){
+       header("Location: admin-inventory.php");
+   }
+}
 
 ?>
