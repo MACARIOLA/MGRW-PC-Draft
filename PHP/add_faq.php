4e2265006edec8b3b5d3a483e0e9aa1fe527f4be
John Rod T. Templo <116851987+Rodpengesabaw@users.noreply.github.com>
2024-05-16 20:58:04 +0800

    Add files via upload

---
+++ PHP/add_faq.php
@@ -0,0 +1,34 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mgwrpcdtb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+$new_question = $_POST["new_question"];
+$new_answer = $_POST["new_answer"];
+
+// Insert new question and answer into the database
+$sql = "INSERT INTO faqs (question, answer) VALUES ('$new_question', '$new_answer')";
+
+if ($conn->query($sql) === TRUE) {
+    // Data inserted successfully, redirect back to cms.php
+    header("Location: ../cms.php");
+    // Show success message on cms.php
+    exit;
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+?>
