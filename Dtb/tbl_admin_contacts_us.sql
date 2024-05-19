901286cf9312bb05111daa5d41a808a3b2960b13
Katrina Ibias <116424991+Ktrncssndrti@users.noreply.github.com>
2024-05-19 09:58:08 +0800

    Update tbl_admin_contacts_us.sql

--- Dtb/tbl_admin_contacts_us.sql
+++ Dtb/tbl_admin_contacts_us.sql
@@ -1,63 +1,24 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 17, 2024 at 08:13 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE `tbl_admin_contacts_us` (
+  `id` int(11) NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `mgwrpcdtb`
---
+INSERT INTO `tbl_admin_contacts_us` (`id`, `customer_name`, `email`, `subject`, `message`, `created_at`) VALUES
+(1, 'Katrina Ibias', 'katrinaibias17@gmail.com', 'asdefrgt', 'gsb htrjwryvhqarere', '2024-05-18 23:00:14'),
+(2, 'Katrina Ibias', 'katrinaibias17@gmail.com', 'asdefrgt', 'gsb htrjwryvhqarere', '2024-05-18 23:00:23'),
+(3, 'Katrina Ibias', 'katrinaibias17@gmail.com', 'asdefrgt', 'gsb htrjwryvhqarere', '2024-05-18 23:08:09'),
+(4, 'Katrina Ibias', 'katrinaibias17@yahoo.com', 'rjhgbwvkerg', 'rgvune[mghpcqpw', '2024-05-18 23:27:19'),
+(5, 'Katrina Ibias', 'katrinaibias17@gmail.com', 'rkthgwntve/5tuphe', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque justo lacus, condimentum ut enim molestie, viverra molestie urna. Duis ac enim ornare, facilisis massa id, sagittis dolor. Vivamus euismod felis a mattis egestas. Etiam risus nisl, placerat ut vulputate ut, volutpat et diam. ', '2024-05-19 00:15:57'),
+(6, 'Katrina Ibias', 'katrinaibias17@gmail.com', 'duhfbgweunyg', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque justo lacus, condimentum ut enim molestie, viverra molestie urna. Duis ac enim ornare, facilisis massa id, sagittis dolor. Vivamus euismod felis a mattis egestas. Etiam risus nisl, placerat ut vulputate ut, volutpat et diam. ', '2024-05-19 01:35:05'),
+(7, 'Katrina Ibias', 'katrinaibias17@gmail.com', 'rjhgbwvkerg', 'ZSeXRCTVYBNUNM,OIO', '2024-05-19 01:52:20');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_admin_contacts_us`
---
-
-CREATE TABLE tbl_admin_contacts_us (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    subject VARCHAR(255),
-    message TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
- 
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tbl_admin_contacts_us`
---
 ALTER TABLE `tbl_admin_contacts_us`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tbl_admin_contacts_us`
---
-ALTER TABLE `tbl_admin_contacts_us`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
