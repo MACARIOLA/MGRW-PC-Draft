bf0c9e8b8da0265549aa8702c735f9414782f244
Jessalyn Macariola <116423257+MACARIOLA@users.noreply.github.com>
2024-05-20 10:24:21 +0800

    Update MgwrPcDtb.sql

--- Dtb/MgwrPcDtb.sql
+++ Dtb/MgwrPcDtb.sql
@@ -85,26 +85,19 @@ CREATE TABLE IF NOT EXISTS cms_pricelist_pdfdownloads (
 -- -----------< FORMS >----------- --
 
 -- PRODUCT REVIEWS --
-CREATE TABLE IF NOT EXISTS customer_product_reviews (
-  submission_date timestamp NOT NULL DEFAULT current_timestamp(),
-  id int(11) NOT NULL,
-  name varchar(255) NOT NULL,
-  comment text DEFAULT NULL,
-  rating enum('5 Star','4 Star','3 Star','2 Star','1 Star') NOT NULL,
-  first_time_buyer tinyint(1) DEFAULT 0,
-  regular_customer tinyint(1) DEFAULT 0,
-  budget_shopper tinyint(1) DEFAULT 0,
-  brand_loyalist tinyint(1) DEFAULT 0,
-  gift_shopper tinyint(1) DEFAULT 0,
-  window_shopper tinyint(1) DEFAULT 0
+CREATE TABLE `customer_product_reviews` (
+  `submission_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `comment` text DEFAULT NULL,
+  `rating` enum('5 Star','4 Star','3 Star','2 Star','1 Star') NOT NULL,
+  `first_time_buyer` tinyint(1) DEFAULT 0,
+  `regular_customer` tinyint(1) DEFAULT 0,
+  `budget_shopper` tinyint(1) DEFAULT 0,
+  `brand_loyalist` tinyint(1) DEFAULT 0,
+  `gift_shopper` tinyint(1) DEFAULT 0,
+  `window_shopper` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-ALTER TABLE `customer_product_reviews`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `customer_product_reviews`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-COMMIT;
 
 
 
