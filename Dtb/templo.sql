695071e1bdb23108280ba41dae221689c73f4fbd
John Rod T. Templo <116851987+Rodpengesabaw@users.noreply.github.com>
2024-05-22 07:32:55 +0800

    Update templo.sql

--- Dtb/templo.sql
+++ Dtb/templo.sql
@@ -1,50 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 20, 2024 at 03:48 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- ------------------------------- --
+-- -< CREATING MGWR PC DATABASE >- --
+
+CREATE DATABASE IF NOT EXISTS MgwrPcDtb;
+USE MgwrPcDtb;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `mgwrpcdtb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin_login`
---
-
-CREATE TABLE `admin_login` (
-  `admin_username` varchar(255) NOT NULL,
-  `admin_password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `admin_login`
---
-
-INSERT INTO `admin_login` (`admin_username`, `admin_password`) VALUES
-('MGWRPCAdmin', 'Admin123');
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
--- --------------------------------------------------------
+-- -< CREATING MGWR PC DATABASE >- --
+-- ------------------------------- --
 
 --
 -- Table structure for table `cms_pricelist_laptop`
@@ -90,12 +55,12 @@ CREATE TABLE `cms_pricelist_printer` (
 
 -- --------------------------------------------------------
 
-
+--
 -- Table structure for table `cms_faqs`
 --
 
 CREATE TABLE `cms_faqs` (
-  `id` int(255) NOT NULL,
+  `id` int NOT NULL,
   `question` varchar(1000) NOT NULL,
   `answer` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -106,9 +71,9 @@ CREATE TABLE `cms_faqs` (
 
 INSERT INTO `cms_faqs` (`id`, `question`, `answer`) VALUES
 (1, 'What payment methods do you accept?', 'We Accept Cash, Online Payment And Bank Transfer.\r\nFor Online Payment And Bank Transfer Please Use Official MGWR PC Bank Account:\r\n\r\nUnion Bank\r\nAccount Name: MGWR PC COMPUTER PARTS AND ACCESSORIES SHOP\r\nAccount Number: 0027-4001-3341\r\n\r\nBank of the Philippines Island\r\nAccount Name: MGWR PC COMPUTER PARTS AND ACCESSORIES SHOP\r\nAccount Number: 0989-644-661'),
-(2, 'Do you offer financing options for purchasing computers?', 'We Accept Product Installment Via Home Credit And Credit Card.\r\n\r\nPreferred Bank For Credit Card Installment.\r\n•   Bank of the philippines Island\r\n•   HSCBC\r\n•   China Bank'),
-(3, 'How Do I apply in Home Credit?', 'You Can Apply In Home Credit In Just Simple Way:\r\n\r\n•   Download \"My Home Credit App\" (Not compatible in IOS)\r\n•   Create an account and kindly fill up all required information\r\n•   Check if you are qualified for product loand by answering those necessary question.\r\n•   If you already finished those steps. Home credit will give you an offer for upto 60k product loand and together with your m Valid ID you can now go to our shop to process your loan.'),
-(4, 'Are your products covered by warranty?', 'All MGWR PC Products Carry A Standard One (1) Year Warranty Except Of The Following:\r\n\r\nOne Month Warranty Applies Only For:\r\n•   External batteries of Laptops\r\n•   Bundled Items ( mouse, keyboards, speacker,etc.)\r\n•   Chargers, Adaptors\r\n\r\nOne Week Warranty Applies Only For:\r\n•   Tables and Chairs\r\n•   Generic Fans,Laptop coolers,Heatsink, LAN tester\r\n•   Powercord , Display Cables\r\n\r\nThere Is No Warranty For:\r\n•   Consumables (ink,disc, etc)\r\n•   Software ( OS, Office)\r\n•   Acessories, promotional/sale items, freebies,cables');
+(2, 'Do you offer financing options for purchasing computers?', 'We Accept Product Installment Via Home Credit And Credit Card.\r\n\r\nPreferred Bank For Credit Card Installment.\r\n•   Bank of the Philippines Island\r\n•   HSBC\r\n•   China Bank'),
+(3, 'How Do I apply in Home Credit?', 'You Can Apply In Home Credit In Just Simple Way:\r\n\r\n•   Download "My Home Credit App" (Not compatible in iOS)\r\n•   Create an account and kindly fill up all required information\r\n•   Check if you are qualified for product loan by answering those necessary questions.\r\n•   If you already finished those steps, Home Credit will give you an offer for up to 60k product loan and together with your valid ID you can now go to our shop to process your loan.'),
+(4, 'Are your products covered by warranty?', 'All MGWR PC Products Carry A Standard One (1) Year Warranty Except Of The Following:\r\n\r\nOne Month Warranty Applies Only For:\r\n•   External batteries of Laptops\r\n•   Bundled Items (mouse, keyboards, speakers, etc.)\r\n•   Chargers, Adaptors\r\n\r\nOne Week Warranty Applies Only For:\r\n•   Tables and Chairs\r\n•   Generic Fans, Laptop coolers, Heatsink, LAN tester\r\n•   Powercord, Display Cables\r\n\r\nThere Is No Warranty For:\r\n•   Consumables (ink, disc, etc)\r\n•   Software (OS, Office)\r\n•   Accessories, promotional/sale items, freebies, cables');
 
 --
 -- Indexes for dumped tables
@@ -128,5 +93,6 @@ ALTER TABLE `cms_faqs`
 -- AUTO_INCREMENT for table `cms_faqs`
 --
 ALTER TABLE `cms_faqs`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 COMMIT;
