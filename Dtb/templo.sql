8556bead569eecc83597f7a879e1051478317ba8
John Rod T. Templo <116851987+Rodpengesabaw@users.noreply.github.com>
2024-05-22 00:29:14 +0800

    Update templo.sql

--- Dtb/templo.sql
+++ Dtb/templo.sql
@@ -89,3 +89,44 @@ CREATE TABLE `cms_pricelist_printer` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
+
+
+-- Table structure for table `cms_faqs`
+--
+
+CREATE TABLE `cms_faqs` (
+  `id` int(255) NOT NULL,
+  `question` varchar(1000) NOT NULL,
+  `answer` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cms_faqs`
+--
+
+INSERT INTO `cms_faqs` (`id`, `question`, `answer`) VALUES
+(1, 'What payment methods do you accept?', 'We Accept Cash, Online Payment And Bank Transfer.\r\nFor Online Payment And Bank Transfer Please Use Official MGWR PC Bank Account:\r\n\r\nUnion Bank\r\nAccount Name: MGWR PC COMPUTER PARTS AND ACCESSORIES SHOP\r\nAccount Number: 0027-4001-3341\r\n\r\nBank of the Philippines Island\r\nAccount Name: MGWR PC COMPUTER PARTS AND ACCESSORIES SHOP\r\nAccount Number: 0989-644-661'),
+(2, 'Do you offer financing options for purchasing computers?', 'We Accept Product Installment Via Home Credit And Credit Card.\r\n\r\nPreferred Bank For Credit Card Installment.\r\n•   Bank of the philippines Island\r\n•   HSCBC\r\n•   China Bank'),
+(3, 'How Do I apply in Home Credit?', 'You Can Apply In Home Credit In Just Simple Way:\r\n\r\n•   Download \"My Home Credit App\" (Not compatible in IOS)\r\n•   Create an account and kindly fill up all required information\r\n•   Check if you are qualified for product loand by answering those necessary question.\r\n•   If you already finished those steps. Home credit will give you an offer for upto 60k product loand and together with your m Valid ID you can now go to our shop to process your loan.'),
+(4, 'Are your products covered by warranty?', 'All MGWR PC Products Carry A Standard One (1) Year Warranty Except Of The Following:\r\n\r\nOne Month Warranty Applies Only For:\r\n•   External batteries of Laptops\r\n•   Bundled Items ( mouse, keyboards, speacker,etc.)\r\n•   Chargers, Adaptors\r\n\r\nOne Week Warranty Applies Only For:\r\n•   Tables and Chairs\r\n•   Generic Fans,Laptop coolers,Heatsink, LAN tester\r\n•   Powercord , Display Cables\r\n\r\nThere Is No Warranty For:\r\n•   Consumables (ink,disc, etc)\r\n•   Software ( OS, Office)\r\n•   Acessories, promotional/sale items, freebies,cables');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cms_faqs`
+--
+ALTER TABLE `cms_faqs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cms_faqs`
+--
+ALTER TABLE `cms_faqs`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
