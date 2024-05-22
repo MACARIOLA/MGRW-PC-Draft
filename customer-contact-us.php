e3e0b1322ad17c36a0e3f637091a9733cfc86982
Joshua Alen Lopez <118281707+Joshieepooo@users.noreply.github.com>
2024-05-22 08:57:45 +0800

    Update customer-contact-us.php

--- customer-contact-us.php
+++ customer-contact-us.php
@@ -49,7 +49,7 @@
                 <a href="home.php" style="--i:0">Home</a>
                 <a href="aboutus.php" style="--i:1">About Us</a>
                 <a href="pricelist.html" style="--i:2">Pricelist</a> 
-                <a href="feedback.php" style="--i:3">Feedbacks</a>
+                <a href="customer-feedback.php" style="--i:3">Feedbacks</a>
                 <a href="faqs.php" style="--i:4">FAQs</a> 
             </nav>
         </header>        
