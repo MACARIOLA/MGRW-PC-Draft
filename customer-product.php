fa1f96e15a8d982b9f6e6623094e9c6daaa93b3b
11Boomie <154297153+11Boomie@users.noreply.github.com>
2024-05-24 08:48:56 +0800

    Update customer-product.php

--- customer-product.php
+++ customer-product.php
@@ -1,22 +1,23 @@
-<!---------------
-       PHP
----------------->
 <?php
-    include 'PHP/reserve.php';
-    include 'PHP/con_db.php';
+include 'PHP/reserve.php';
+include 'PHP/con_db.php';
 
-    if (isset($_GET['id'])) {
-        $_SESSION['product_id'] = $_GET['id'];
-        header("Location: customer-product.php");
-        exit();
-    }
+// Check if 'id' is passed via GET and set the session variable
+if (isset($_GET['id'])) {
+    $_SESSION['product_id'] = $_GET['id'];
+    // Redirect to avoid resubmission of the GET request
+    header("Location: product.php");
+    exit();
+}
 
-    if (isset($_SESSION['product_id'])) {
-        $product_id = $_SESSION['product_id'];
-        $sql = "SELECT * FROM inventory WHERE id = $product_id";
-        $result = mysqli_query($conn, $sql);
-        $product = mysqli_fetch_assoc($result);
-    }   
+// Retrieve the product ID from the session
+if (isset($_SESSION['product_id'])) {
+    $product_id = $_SESSION['product_id'];
+    $sql = "SELECT * FROM inventory WHERE id = $product_id";
+    $result = mysqli_query($conn, $sql);
+    $product = mysqli_fetch_assoc($result);
+    
+}
 ?>
 
 
@@ -36,7 +37,7 @@
          CSS & JS
     ---------------->
     <link rel="stylesheet" href="css/mainstyle.css">
-    <link rel="stylesheet" href="css/customer-product.css">
+    <link rel="stylesheet" href="css/product.css">
     <script type="text/javascript" src="js/mainscript.js"></script>
 
     <!---------------
@@ -57,7 +58,7 @@
           NAVBAR
     ---------------->
     <header class="header">
-        <a href="customer-home.php"><img src="Images/MGWR PC Logo.png" alt="" class="logo"></a>
+        <a href="home.html"><img src="Images/MGWR PC Logo.png" alt="" class="logo"></a>
 
         <input type="checkbox" id="check">
         <label for="check" class="icons">
@@ -66,11 +67,11 @@
         </label>
 
         <nav class="navbar">
-            <a href="customer-home.php" style="--i:0">Home</a>
-            <a href="customer-aboutus.php" style="--i:1">About Us</a>
-            <a href="customer-pricelist.html" style="--i:2">Pricelist</a> 
-            <a href="customer-feedback.php" style="--i:3">Feedbacks</a>
-            <a href="customer-faqs.php" style="--i:4">FAQs</a> 
+            <a href="home.html" style="--i:0">Home</a>
+            <a href="aboutus.html" style="--i:1">About Us</a>
+            <a href="pricelist.html" style="--i:2">Pricelist</a> 
+            <a href="feedback.html" style="--i:3">Feedbacks</a>
+            <a href="faqs.html" style="--i:4">FAQs</a> 
         </nav>
     </header>     
 
@@ -78,61 +79,58 @@
 
     <!---------------
           PRODUCT
-    ----------------> 
-    `<section class="about">
-        <div class="main">
-            <div class="image-container">
-                <div class="hover-image">
-                    <img src="data:image;base64,<?php echo base64_encode($product['image']); ?>" alt="">
-                </div>
-                <div class="btn">
-                    <button type="button" class="btn2" id="reserveNowBtn">Reserve Now</button>
-                </div>
+    ---------------->
+   
+<section class="about">
+    <div class="main">
+        <div class="image-container">
+            <div class="hover-image">
+                <img src="data:image;base64,<?php echo base64_encode($product['image']); ?>" alt="">
             </div>
-            <div class="all-text">
-                <h4><?php echo $product['products_name']; ?></h4>
-                <h1><?php echo $product['products_id']; ?></h1>
-                <div class="price">₱<?php echo $product['unit_price']; ?></div>
-                <p><?php echo $product['description']; ?></p>
-                <div class="features">
-                    <div class="row">
-                        <img class="icn" src="Images/icon-cpu.png" alt="">
-                        <div class="column c1"><?php echo $product['specs_cpu']; ?></div>
-                        <img class="icn" src="Images/icon-mb.png" alt="">
-                        <div class="column c2"><?php echo $product['specs_motherboard']; ?></div>
-                    </div>
-                    <div class="row">
-                        <img class="icn" src="Images/icon-ram.png" alt="">
-                        <div class="column c3"><?php echo $product['specs_ram']; ?></div>
-                        <img class="icn" src="Images/icon-mntr.png" alt="">
-                        <div class="column c4"><?php echo $product['specs_monitor']; ?></div>
-                    </div>
-                    <div class="row">
-                        <img class="icn" src="Images/icon-ssd.png" alt="">
-                        <div class="column c5"><?php echo $product['specs_ssd']; ?></div>
-                        <img class="icn" src="Images/icon-cc.png" alt="">
-                        <div class="column c6"><?php echo $product['specs_computercase']; ?></div>
-                    </div>
-                    <div class="row">
-                        <img class="icn" src="Images/icon-ps.png" alt="">
-                        <div class="column c7"><?php echo $product['specs_powersupply']; ?></div>
-                        <img class="icn" src="Images/icon-fan.png" alt="">
-                        <div class="column c8"><?php echo $product['specs_fan']; ?></div>
-                    </div>
+            <div class="btn">
+                <button type="button" class="btn2" id="reserveNowBtn">Reserve Now</button>
+            </div>
+        </div>
+        <div class="all-text">
+            <h4><?php echo $product['products_name']; ?></h4>
+            <h1><?php echo $product['products_id']; ?></h1>
+            <div class="price">₱<?php echo $product['unit_price']; ?></div>
+            <p><?php echo $product['description']; ?></p>
+            <div class="features">
+                <div class="row">
+                    <img class="icn" src="Images/icon-cpu.png" alt="">
+                    <div class="column c1"><?php echo $product['specs_cpu']; ?></div>
+                    <img class="icn" src="Images/icon-mb.png" alt="">
+                    <div class="column c2"><?php echo $product['specs_motherboard']; ?></div>
+                </div>
+                <div class="row">
+                    <img class="icn" src="Images/icon-ram.png" alt="">
+                    <div class="column c3"><?php echo $product['specs_ram']; ?></div>
+                    <img class="icn" src="Images/icon-mntr.png" alt="">
+                    <div class="column c4"><?php echo $product['specs_monitor']; ?></div>
+                </div>
+                <div class="row">
+                    <img class="icn" src="Images/icon-ssd.png" alt="">
+                    <div class="column c5"><?php echo $product['specs_ssd']; ?></div>
+                    <img class="icn" src="Images/icon-cc.png" alt="">
+                    <div class="column c6"><?php echo $product['specs_computercase']; ?></div>
+                </div>
+                <div class="row">
+                    <img class="icn" src="Images/icon-ps.png" alt="">
+                    <div class="column c7"><?php echo $product['specs_powersupply']; ?></div>
+                    <img class="icn" src="Images/icon-fan.png" alt="">
+                    <div class="column c8"><?php echo $product['specs_fan']; ?></div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
 
-
-    <!---------------
-          POPUP
-    ---------------->
-    <div id="reserveModal" class="modal">
+<div id="reserveModal" class="modal">
         <div class="modal-content">
             <h2>Reservation Details</h2>
-            <form id="reserveForm" method="POST" action="reserve.php">
+            <form id="reserveForm" method="POST">
                 <label for="fName">First Name</label>
                 <input type="text" id="fName" name="fname" placeholder="Enter First Name" required>
                 <label for="lName">Last Name</label>
@@ -159,9 +157,16 @@
         </div>
     </div>
 
-    <!---------------
-        INLINE JS
-    ---------------->
+    <?php if(isset($successMessage)): ?>
+    <script>
+        document.getElementById("successModal").style.display = "block";
+    </script>
+    <?php elseif(isset($errorMessage)): ?>
+    <script>
+        alert('<?php echo $errorMessage; ?>');
+    </script>
+    <?php endif; ?>
+
     <script>
         document.getElementById("reserveNowBtn").addEventListener("click", function() {
             document.getElementById("reserveModal").style.display = "block";
@@ -173,18 +178,22 @@
 
         document.getElementById("goBackBtn").addEventListener("click", function() {
             document.getElementById("successModal").style.display = "none";
-            window.location.href = "home.html";
+            window.location.href = "home.php";
+        });
+
+        document.querySelectorAll(".close-btn").forEach(btn => {
+            btn.addEventListener("click", function() {
+                this.closest(".modal").style.display = "none";
+            });
         });
     </script>
-
-
 
     <!---------------
        CONTACT US
     ---------------->
     <div class="contactus">
         <p>Leave A Feedback For The Developers</p>
-        <a class="btn-fdbck" id="leaveFeedbackBtn" href="customer-survey.php">Questionaires</a>
+        <a class="btn-fdbck" id="leaveFeedbackBtn" href="survey.html">Questionaires</a>
     </div>
 
 
@@ -201,16 +210,16 @@
         <div class="footer-content">
             <h4>Company</h4>
             <ul>    
-                <li><a href="customer-contact-us.php">Contact Us</a></li>
-                <li><a href="customer-faqs.php">FAQs</a></li>
+                <li><a href="contact.html">Contact Us</a></li>
+                <li><a href="faqs.html">FAQs</a></li>
             </ul>
         </div>
         
         <div class="footer-content">
             <h4>Help</h4>
             <ul>
-                <li><a href="customer-privacy.html">Privacy Policy</a></li>                
-                <li><a href="customer-terms.html">Terms of Service</a></li>
+                <li><a href="privacy.html">Privacy Policy</a></li>                
+                <li><a href="term.html">Terms of Service</a></li>
             </ul>
         </div>
  
@@ -218,8 +227,8 @@
             <h4>Connect With Us</h4>
             <div class="icons2">
                 <a href="https://www.facebook.com/mgwrpc"><img src="Images/fbicon.png" alt=""></a>
-                <a href="https://www.instagram.com/mgwrpc"><img src="Images/igicon.png" alt=""></a>
-                <a href="https://www.tiktok.com/@mgwrpctrading"><img src="Images/tticon.png" alt=""></a>
+                <a href="https://www.instagram.com/mgwrpc?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><img src="Images/igicon.png" alt=""></a>
+                <a href="https://www.tiktok.com/@mgwrpctrading?is_from_webapp=1&sender_device=pc"><img src="Images/tticon.png" alt=""></a>
             </div>
         </div>
     </footer>
