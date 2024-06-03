22f138573904ec94443496704b318b5ad1947d10
Jessalyn Macariola <116423257+MACARIOLA@users.noreply.github.com>
2024-06-03 12:03:41 +0800

    Update insert_product.php

--- PHP/insert_product.php
+++ PHP/insert_product.php
@@ -14,17 +14,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update_product'])) {
     // Retrieve and sanitize input values
     $product_products_id = mysqli_real_escape_string($conn, $_POST['product_products_id']);
     $product_name = mysqli_real_escape_string($conn, $_POST['product_name']);
-    $product_description = mysqli_real_escape_string($conn, $_POST['product_description']);
     $product_unit_price = mysqli_real_escape_string($conn, $_POST['product_unit_price']);
     $product_total_units = intval($_POST['product_total_units']);
-    $specs_cpu = mysqli_real_escape_string($conn, $_POST['specs_cpu']);
-    $specs_motherboard = mysqli_real_escape_string($conn, $_POST['specs_motherboard']);
-    $specs_ram = mysqli_real_escape_string($conn, $_POST['specs_ram']);
-    $specs_ssd = mysqli_real_escape_string($conn, $_POST['specs_ssd']);
-    $specs_monitor = mysqli_real_escape_string($conn, $_POST['specs_monitor']);
-    $specs_computercase = mysqli_real_escape_string($conn, $_POST['specs_computercase']);
-    $specs_powersupply = mysqli_real_escape_string($conn, $_POST['specs_powersupply']);
-    $specs_fan = mysqli_real_escape_string($conn, $_POST['specs_fan']);
 
     // Handle the image file upload
     if (isset($_FILES['product_image']) && $_FILES['product_image']['error'] == 0) {
@@ -37,15 +28,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update_product'])) {
 
     // Prepare and bind
     $stmt = $conn->prepare("INSERT INTO inventory
-    (products_id, products_name, image, description, unit_price, total_units, reserved_units, specs_cpu, specs_motherboard, specs_ram, specs_ssd, specs_monitor, specs_computercase, specs_powersupply, specs_fan)
-    VALUES (?, ?, ?, ?, ?, ?, 0, ?, ?, ?, ?, ?, ?, ?, ?)");
+    (products_id,
+    products_name,
+    image,
+    unit_price,
+    total_units,
+    reserved_units)
+    VALUES (?, ?, ?, ?, ?, 0)");
 
     // Check if prepare() failed
     if (!$stmt) {
         die("Prepare failed: " . $conn->error);
     }
 
-    $stmt->bind_param("sssdiissssssss", $product_products_id, $product_name, $product_image, $product_description, $product_unit_price, $product_total_units, $specs_cpu, $specs_motherboard, $specs_ram, $specs_ssd, $specs_monitor, $specs_computercase, $specs_powersupply, $specs_fan);
+    $stmt->bind_param("sssdi",
+    $product_products_id,
+    $product_name,
+    $product_image,
+    $product_unit_price,
+    $product_total_units);
 
     // Execute the statement
     if ($stmt->execute()) {
