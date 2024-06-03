70d8e3866aff545d3df95957f93b756b71c60abb
Jessalyn Macariola <116423257+MACARIOLA@users.noreply.github.com>
2024-06-03 12:04:06 +0800

    Update admin-update.php

--- PHP/admin-update.php
+++ PHP/admin-update.php
@@ -10,16 +10,7 @@ if (isset($_POST['update_product'])) {
     $product_image_size = $_FILES['product_image']['size'];
     $product_total_units = $_POST['product_total_units'];
     $product_reserved_units = $_POST['product_reserved_units'];
-    $product_description = $_POST['product_description'];
     $product_unit_price = $_POST['product_unit_price'];
-    $specs_cpu = $_POST['specs_cpu'];
-    $specs_motherboard = $_POST['specs_motherboard'];
-    $specs_ram = $_POST['specs_ram'];
-    $specs_ssd = $_POST['specs_ssd'];
-    $specs_monitor = $_POST['specs_monitor'];
-    $specs_computercase = $_POST['specs_computercase'];
-    $specs_powersupply = $_POST['specs_powersupply'];
-    $specs_fan = $_POST['specs_fan'];
 
     // Check if image file is uploaded
     if (isset($_FILES['product_image']) && is_uploaded_file($_FILES['product_image']['tmp_name'])) {
@@ -27,10 +18,23 @@ if (isset($_POST['update_product'])) {
         $imgContent = addslashes(file_get_contents($_FILES['product_image']['tmp_name']));
 
         // Update SQL query with image data
-        $update_data = "UPDATE inventory SET products_id='$product_products_id', products_name='$product_name', image='$imgContent', total_units='$product_total_units', reserved_units='$product_reserved_units', description='$product_description', unit_price='$product_unit_price', specs_cpu='$specs_cpu', specs_motherboard='$specs_motherboard', specs_ram='$specs_ram', specs_ssd='$specs_ssd', specs_monitor='$specs_monitor', specs_computercase='$specs_computercase', specs_powersupply='$specs_powersupply', specs_fan='$specs_fan' WHERE id = '$product_id'";
+        $update_data = "UPDATE inventory SET
+        products_id='$product_products_id',
+        products_name='$product_name',
+        image='$imgContent',
+        total_units='$product_total_units',
+        reserved_units='$product_reserved_units',
+        unit_price='$product_unit_price'
+        WHERE id = '$product_id'";
     } else {
         // If no image uploaded, update SQL query without image data
-        $update_data = "UPDATE inventory SET products_id='$product_products_id', products_name='$product_name', total_units='$product_total_units', reserved_units='$product_reserved_units', description='$product_description', unit_price='$product_unit_price', specs_cpu='$specs_cpu', specs_motherboard='$specs_motherboard', specs_ram='$specs_ram', specs_ssd='$specs_ssd', specs_monitor='$specs_monitor', specs_computercase='$specs_computercase', specs_powersupply='$specs_powersupply', specs_fan='$specs_fan' WHERE id = '$product_id'";
+        $update_data = "UPDATE inventory SET
+        products_id='$product_products_id',
+        products_name='$product_name',
+        total_units='$product_total_units',
+        reserved_units='$product_reserved_units',
+        unit_price='$product_unit_price'
+        WHERE id = '$product_id'";
     }
 
     // Execute the update query
