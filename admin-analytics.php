2a680d434c60ff05c987b7a116bedbc4f9713b3e
11Boomie <154297153+11Boomie@users.noreply.github.com>
2024-06-04 10:23:05 +0800

    Update admin-analytics.php

--- admin-analytics.php
+++ admin-analytics.php
@@ -4,6 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+ <!----------------
+        PHP
+            ---->
+        <?php
+        session_start();
+        
+        if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+            // Redirect to the login page if not logged in
+            echo "<script>
+                    alert('Unidentified login credentials');
+                    window.location.href = 'admin-login.php';
+                  </script>";
+            exit();
+        }
+        
+        ?>
+
+
     <!---------------
            TAB
     ---------------->
