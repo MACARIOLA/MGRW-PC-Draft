563159e5a2bbe9d54cb574754a9bfc421fad4b1e
11Boomie <154297153+11Boomie@users.noreply.github.com>
2024-06-04 10:27:51 +0800

    Update admin-contact-us.php

--- admin-contact-us.php
+++ admin-contact-us.php
@@ -1,13 +1,30 @@
-<?php
-    include 'php/customer-contact-us.php';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!---------------
+          PHP
+    ---------------->
+
+    <?php
+    include 'php/customer-contact-us.php';
+    ?>
+        <?php
+        session_start();
+        
+        if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+            // Redirect to the login page if not logged in
+            echo "<script>
+                    alert('Unidentified login credentials');
+                    window.location.href = 'admin-login.php';
+                  </script>";
+            exit();
+        }
+        
+        ?>
 
     <!---------------
           TAB
