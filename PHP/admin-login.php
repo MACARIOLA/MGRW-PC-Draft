596435b31358a6ed1d14c0103001e1c5fb856912
11Boomie <154297153+11Boomie@users.noreply.github.com>
2024-06-04 11:00:28 +0800

    Update admin-login.php

--- PHP/admin-login.php
+++ PHP/admin-login.php
@@ -1,14 +1,4 @@
-<?php
-    $dbhost = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "mgwrpcdtb";
 
-    $conn = mysqli_connect($dbhost, $username, $password, $dbname);
-
-    if($conn->connect_error){
-        die("Connection failed" . $conn->connect_error);
-    }
 <?php
 session_start(); // Start the session
 
@@ -55,24 +45,3 @@ $conn->close(); // Close the connection
 }
 ?>
 
-    $admin_username = isset($_POST['admin_username']) ? $_POST['admin_username'] : '';
-    $admin_password = isset($_POST['admin_password']) ? $_POST['admin_password'] : '';
-
-    if (empty($admin_username) || empty($admin_password)) {
-        die("Invalid input data");
-    }
-
-    $sql = "SELECT * FROM admin_login WHERE admin_username = '$admin_username' AND admin_password = '$admin_password'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        header("Location: ../admin-analytics.php");  
-        exit();
-    } else {
-        echo "<script>
-                alert('Invalid login credentials');
-                window.location.href = '../admin-login.html';
-            </script>";
-    }
-    $conn->close();
-?>
